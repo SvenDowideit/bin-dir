e4f4dab71453bc761dad53ce878e461390b9b047
Sven Dowideit <SvenDowideit@home.org.au>
2017-08-08 12:26:20 +1000

    add a run if already installed
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- debian-arm32.sh
+++ debian-arm32.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -ex
 
+# see https://translatedcode.wordpress.com/2016/11/03/installing-debian-on-qemus-32-bit-arm-virt-board/
+
 if [ ! -e installer-vmlinuz ]; then
 	wget -O installer-vmlinuz http://http.us.debian.org/debian/dists/jessie/main/installer-armhf/current/images/netboot/vmlinuz
 fi
@@ -11,13 +13,25 @@ fi
 
 if [ ! -e hda.qcow2 ]; then
 	qemu-img create -f qcow hda.qcow2 10G
-fi
 
-qemu-system-arm -M virt -m 4096 \
-	  -kernel installer-vmlinuz \
-	    -initrd installer-initrd.gz \
-	      -drive if=none,file=hda.qcow2,format=qcow,id=hd \
-	        -device virtio-blk-device,drive=hd \
-		  -netdev user,id=mynet \
-		    -device virtio-net-device,netdev=mynet \
-		      -nographic -no-reboot
+	qemu-system-arm -M virt -m 4096 \
+		-kernel installer-vmlinuz \
+		-initrd installer-initrd.gz \
+		-drive if=none,file=hda.qcow2,format=qcow,id=hd \
+		-device virtio-blk-device,drive=hd \
+		-netdev user,id=mynet \
+		-device virtio-net-device,netdev=mynet \
+		-nographic -no-reboot
+	# TODO: copy out the vmlinz and initrd
+	virt-copy-out -a hda.qcow2 /boot/vmlinuz-3.16.0-4-armmp-lpae /boot/initrd.img-3.16.0-4-armmp-lpae .
+else
+	qemu-system-arm -M virt -m 4096 \
+	  -kernel vmlinuz-3.16.0-4-armmp-lpae \
+	  -initrd initrd.img-3.16.0-4-armmp-lpae \
+	  -append 'root=/dev/vda2' \
+	  -drive if=none,file=hda.qcow2,format=qcow,id=hd \
+	  -device virtio-blk-device,drive=hd \
+	  -netdev user,id=mynet \
+	  -device virtio-net-device,netdev=mynet \
+	  -nographic
+fi
